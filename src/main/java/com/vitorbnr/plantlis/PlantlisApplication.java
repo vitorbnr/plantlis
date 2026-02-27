87ce77603c0f47a09c3c8bb0b8ff005eb1067db5
vitorbnr <vitorbernardotech@gmail.com>
2026-02-27 16:22:56 -0300

    first commit

---
+++ src/main/java/com/vitorbnr/plantlis/PlantlisApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.plantlis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantlisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantlisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorbnr/plantlis/PlantlisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.plantlis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantlisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
